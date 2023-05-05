f9f93be62b0943375049117086da1901b380c68f
fikaa <benidiktusvio@gmail.com>
2023-05-06 02:15:01 +0700

    🔥 chore(editorconfig): add .editorconfig file to enforce consistent coding styles ✨ feat(eslintrc.json): add eslint rules to improve code quality and readability 🔧 chore(husky): add pre-commit hook to run linting, formatting, and build scripts before committing 🔧 chore(npmrc): add auto-install-peers=true to automatically install peer dependencies 🔧 chore(prettier): add .prettierignore file to exclude directories from prettier formatting 🔧 chore(prettier): replace .prettierrc with .prettierrc.json to allow for more complex configurations 🔧 chore(prettier): add prettier plugins and configuration to improve code formatting 🔧 chore(next.config.js): remove trailing comma to improve compatibility with older versions of Node.js 🗑️ chore(README.md): remove default Next.js README content 🔧 chore(firebase.json
    🗑️ chore(index.html): remove index.html, next.svg, and vercel.svg
    The files index.html, next.svg, and vercel.svg were removed from the public directory. These files were not being used in the application and were therefore removed to reduce clutter and improve maintainability.
    
    🎨 style(layout.tsx): reformat code to improve readability and consistency
    🎨 style(page.tsx): reformat code to improve readability and consistency
    The changes made in both files are purely cosmetic, aimed at improving the code's readability and consistency. In layout.tsx, the code was reformatted to follow a consistent style, including the use of consistent indentation, spacing, and line breaks. In page.tsx, the code was also reformatted to follow a consistent style, including the use of consistent indentation and spacing.
    
    🎨 style(hero.tsx): reformat code to improve readability and consistency
    🔧 chore(tailwind.config.js): reformat code to improve readability and consistency
    🔧 chore(tsconfig.json): enable strict type checking and remove unused code
    The code in hero.tsx, tailwind.config.js, and tsconfig.json has been reformatted to improve readability and consistency. In tsconfig.json, strict type checking has been enabled and unused code has been removed.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
-import Hero from '@/components/hero';
-import Image from 'next/image';
+import Hero from "@/components/hero";
 
-export default function Home() {
-    return (
-        <main className="flex min-h-screen flex-col items-center justify-center p-2">
-            <Hero></Hero>
-        </main>
-    );
-}
+const Home = () => {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-2">
+      <Hero></Hero>
+    </main>
+  );
+};
+
+export default Home;
