178b5c2fdb3bfe8adf3016f727b3b917b2ab2e6b
fikaa <benidiktusvio@gmail.com>
2023-05-06 01:38:15 +0700

    🎨 style(.prettierrc): add Prettier configuration file 🖼️ add(LOGO_HIMAFORKA.png): add HIMAFORKA logo image ✨ feat(layout.tsx): add navigation bar to layout component 🚚 chore(page.tsx): remove navbar from page component and add Hero component The Prettier configuration file is added to the project to enforce consistent code formatting. The HIMAFORKA logo image is added to the public/img directory. The layout component now includes a navigation bar to improve the user experience and make it easier to navigate the website. The navbar includes links to various pages and subpages of the website. The page component no longer includes a navbar and instead includes a Hero component to improve the visual appeal of the page.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,172 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en">
-            <body>{children}</body>
+            <body>
+                <nav className="navbar bg-base-100">
+                    <div className="flex-1">
+                        <a className="btn btn-ghost normal-case text-xl gap-2">
+                            <div className="w-10 rounded-full">
+                                <img src="/img/LOGO_HIMAFORKA.png" />
+                            </div>
+                            <h2 className="text-sm">HIMAFORKA UAJY</h2>
+                        </a>
+                    </div>
+                    <div className="flex-none">
+                        <ul className="menu menu-horizontal px-1">
+                            <li>
+                                <a>Beranda</a>
+                            </li>
+                            <li tabIndex={0}>
+                                <a>
+                                    Tentang
+                                    <svg
+                                        className="fill-current"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        width="20"
+                                        height="20"
+                                        viewBox="0 0 24 24"
+                                    >
+                                        <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
+                                    </svg>
+                                </a>
+                                <ul className="p-2 bg-base-100">
+                                    <li tabIndex={1}>
+                                        <a>
+                                            Struktur Organisasi
+                                            <svg
+                                                className="fill-current"
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                width="20"
+                                                height="20"
+                                                viewBox="0 0 24 24"
+                                            >
+                                                <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
+                                            </svg>
+                                        </a>
+                                        <ul className="p-2 bg-base-100">
+                                            <li>
+                                                <a>Masa Bakti 2020/2021</a>
+                                            </li>
+                                            <li>
+                                                <a>Masa Bakti 2021/2022</a>
+                                            </li>
+                                            <li>
+                                                <a>Masa Bakti 2022/2023</a>
+                                            </li>
+                                        </ul>
+                                    </li>
+                                    <li>
+                                        <a>Sejarah dan Visi-Misi</a>
+                                    </li>
+                                    <li>
+                                        <a>
+                                            Deskripsi Bidang
+                                            <svg
+                                                className="fill-current"
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                width="20"
+                                                height="20"
+                                                viewBox="0 0 24 24"
+                                            >
+                                                <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
+                                            </svg>
+                                        </a>
+                                        <ul className="p-2 bg-base-100">
+                                            <li>
+                                                <a>Deskripsi Singkat</a>
+                                            </li>
+                                            <li>
+                                                <a>Akademik dan Prestasi</a>
+                                            </li>
+                                            <li>
+                                                <a>Komunikasi dan Informasi</a>
+                                            </li>
+                                            <li>
+                                                <a>Usaha Dana</a>
+                                            </li>
+                                            <li>
+                                                <a>Sumber Daya Manusia</a>
+                                            </li>
+                                        </ul>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a>
+                                    Informasi
+                                    <svg
+                                        className="fill-current"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        width="20"
+                                        height="20"
+                                        viewBox="0 0 24 24"
+                                    >
+                                        <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
+                                    </svg>
+                                </a>
+                                <ul className="p-2 bg-base-100">
+                                    <li tabIndex={2}>
+                                        <ul className="p-2 bg-base-100">
+                                            <li>
+                                                <a>Pengumuman</a>
+                                            </li>
+                                            <li>
+                                                <a>Umum</a>
+                                            </li>
+                                            <li>
+                                                <a>
+                                                    Berita
+                                                    <svg
+                                                        className="fill-current"
+                                                        xmlns="http://www.w3.org/2000/svg"
+                                                        width="20"
+                                                        height="20"
+                                                        viewBox="0 0 24 24"
+                                                    >
+                                                        <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
+                                                    </svg>
+                                                </a>
+                                                <ul className="p-2 bg-base-100">
+                                                    <li tabIndex={2}>
+                                                        <ul className="p-2 bg-base-100">
+                                                            <li>
+                                                                <a>Akademik</a>
+                                                            </li>
+                                                            <li>
+                                                                <a>Non-Akademik</a>
+                                                            </li>
+                                                        </ul>
+                                                    </li>
+                                                </ul>
+                                            </li>
+                                        </ul>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li tabIndex={0}>
+                                <a>
+                                    Galeri
+                                    <svg
+                                        className="fill-current"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        width="20"
+                                        height="20"
+                                        viewBox="0 0 24 24"
+                                    >
+                                        <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
+                                    </svg>
+                                </a>
+                                <ul className="p-2 bg-base-100">
+                                    <li>
+                                        <a>Himaforka '20-21'</a>
+                                    </li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+                {children}
+            </body>
         </html>
     );
 }
---
+++ src/components/hero.tsx
@@ -0,0 +1,21 @@
+export default function Hero() {
+    return (
+        <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content flex justify-center">
+                <div className="items-center flex-col justify-center">
+                    <h1 className="text-5xl font-bold text-center">HIMAFORKA UAJY</h1>
+                    <h2 className="text-5xl font-bold text-center">UNIVERSITAS ATMA JAYA YOGYAKARTA</h2>
+                    <p className="py-6 text-center">
+                        Lembaga kemahasiswaan terstruktur di tingkat program studi yang menampung, menyalurkan, serta
+                        menjadi wadah aspirasi dan sarana pengembangan potensi mahasiswa Informatika Fakultas Teknologi
+                        Industri Universitas Atma Jaya Yogyakarta.
+                    </p>
+                    <div className="flex justify-center gap-4">
+                        <button className="btn btn-primary">Get Started</button>
+                        <button className="btn btn-primary">Get Started</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
