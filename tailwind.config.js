d27296413c2abc4f49400e6875580e8f2d52f3c1
rielgbrl <rielgasy9@gmail.com>
2023-05-26 18:20:46 +0700

    🎨 style(layout.tsx): change single quotes to double quotes for metadata object keys 🎨 style(layout.tsx): change single quotes to double quotes for import statements 🎨 style(navigation.tsx): change indentation to 4 spaces and reformat code for better readability 🎨 style(hero.tsx): change indentation to 4 spaces and reformat code for better readability The changes made in this commit are purely cosmetic. The single quotes in the metadata object keys and import statements were changed to double quotes for consistency with the rest of the codebase. The indentation and formatting of the code in navigation.tsx and hero.tsx were also improved for better readability.
    🎨 style(tailwind.config.js): reformat code and add a custom theme for daisyui
    The code has been reformatted to improve readability. A custom theme has been added for daisyui, which includes new color values for primary, secondary, accent, neutral, base-100, info, success, warning, and error. This allows for a more personalized and consistent look and feel across the application.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    content: [
+        './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+    daisyui: {
+        themes: [
+            {
+                mytheme: {
+                    primary: '#65C3C8',
+
+                    secondary: '#EF9FBC',
+
+                    accent: '#EEAF3A',
+
+                    neutral: '#291334',
+
+                    'base-100': '#FAF7F5',
+
+                    info: '#3ABFF8',
+
+                    success: '#36D399',
+
+                    warning: '#FBBD23',
+
+                    error: '#F87272',
+                },
+            },
+        ],
     },
-  },
-  plugins: [require("daisyui")],
+    plugins: [require('daisyui')],
 };
